80006952547c775362f5ec9c2e2666269cc63198
Mario Fernandez <mario.fernandez@hceris.com>
2020-02-08 13:23:05 +0100

    fix the infra tests

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -2,12 +2,12 @@ require_relative 'spec_helper'
 
 describe 'Container' do
   describe file('/etc/alpine-release') do
-    its(:content) { is_expected.to match(/3.9/) }
+    its(:content) { is_expected.to match(/3.11/) }
   end
 
   describe 'node' do
     describe command('node -v') do
-      its(:stdout) { is_expected.to match(/12.6/) }
+      its(:stdout) { is_expected.to match(/13.6/) }
     end
   end
 
